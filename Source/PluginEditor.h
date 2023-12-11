8f42978d47fc9e596af888a38612f58f954d627a
Gl4SS3S <jthurling78@gmail.com>
2023-12-11 20:31:12 +0200

    Resync .gitignore

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class GlideAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    GlideAudioProcessorEditor (GlideAudioProcessor&);
+    ~GlideAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    GlideAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GlideAudioProcessorEditor)
+};
